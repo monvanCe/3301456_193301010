2f2845d8b59a31c412212ec7360a1573a60417b9
monvanCe <omerfkoca@gmail.com>
2022-04-06 05:49:47 +0300

    navbar ico completed - news files added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'insertnews.dart';
+import 'news/news1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,6 +40,8 @@ class _HomePageState extends State<HomePage> {
     const Page2(),
     const Page3(),
     const Page4(),
+    const Page5(),
+    const Page6(),
   ];
 
   @override
@@ -69,7 +71,7 @@ class _HomePageState extends State<HomePage> {
 
   Container buildMyNavBar(BuildContext context) {
     return Container(
-      height: 50,
+      height: 70,
       decoration: BoxDecoration(
         color: Theme.of(context).primaryColor,
         borderRadius: const BorderRadius.only(
@@ -80,81 +82,155 @@ class _HomePageState extends State<HomePage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          IconButton(
-            enableFeedback: false,
-            onPressed: () {
-              setState(() {
-                pageIndex = 0;
-              });
-            },
-            icon: pageIndex == 0
-                ? const Icon(
-                    Icons.home_filled,
-                    color: Colors.white,
-                    size: 35,
-                  )
-                : const Icon(
-                    Icons.home_outlined,
-                    color: Colors.white,
-                    size: 35,
-                  ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 0;
+                  });
+                },
+                icon: pageIndex == 0
+                    ? const Icon(
+                        Icons.home_filled,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.home_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+              Text('Ana Sayfa'),
+            ],
           ),
-          IconButton(
-            enableFeedback: false,
-            onPressed: () {
-              setState(() {
-                pageIndex = 1;
-              });
-            },
-            icon: pageIndex == 1
-                ? const Icon(
-                    Icons.work_rounded,
-                    color: Colors.white,
-                    size: 35,
-                  )
-                : const Icon(
-                    Icons.work_outline_outlined,
-                    color: Colors.white,
-                    size: 35,
-                  ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 1;
+                  });
+                },
+                icon: pageIndex == 1
+                    ? const Icon(
+                        Icons.whatshot,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.whatshot_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+              Text('Son Dakika'),
+            ],
           ),
-          IconButton(
-            enableFeedback: false,
-            onPressed: () {
-              setState(() {
-                pageIndex = 2;
-              });
-            },
-            icon: pageIndex == 2
-                ? const Icon(
-                    Icons.widgets_rounded,
-                    color: Colors.white,
-                    size: 35,
-                  )
-                : const Icon(
-                    Icons.widgets_outlined,
-                    color: Colors.white,
-                    size: 35,
-                  ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 2;
+                  });
+                },
+                icon: pageIndex == 2
+                    ? const Icon(
+                        Icons.monetization_on,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.monetization_on_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+              Text('Ekonomi'),
+            ],
           ),
-          IconButton(
-            enableFeedback: false,
-            onPressed: () {
-              setState(() {
-                pageIndex = 3;
-              });
-            },
-            icon: pageIndex == 3
-                ? const Icon(
-                    Icons.person,
-                    color: Colors.white,
-                    size: 35,
-                  )
-                : const Icon(
-                    Icons.person_outline,
-                    color: Colors.white,
-                    size: 35,
-                  ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 3;
+                  });
+                },
+                icon: pageIndex == 3
+                    ? const Icon(
+                        Icons.sports_basketball,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.sports_basketball_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+              Text('Spor'),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 4;
+                  });
+                },
+                icon: pageIndex == 4
+                    ? const Icon(
+                        Icons.health_and_safety,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.health_and_safety_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+              Text('Sağlık'),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    pageIndex = 5;
+                  });
+                },
+                icon: pageIndex == 5
+                    ? const Icon(
+                        Icons.settings,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : const Icon(
+                        Icons.settings_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+              ),
+              Text('Ayarlar'),
+            ],
           ),
         ],
       ),
@@ -415,6 +491,48 @@ class Page4 extends StatelessWidget {
       child: Center(
         child: Text(
           "Page Number 4",
+          style: TextStyle(
+            color: Colors.green[900],
+            fontSize: 45,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Page5 extends StatelessWidget {
+  const Page5({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromARGB(255, 211, 168, 168),
+      child: Center(
+        child: Text(
+          "Page Number 5",
+          style: TextStyle(
+            color: Colors.green[900],
+            fontSize: 45,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Page6 extends StatelessWidget {
+  const Page6({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromARGB(255, 211, 168, 168),
+      child: Center(
+        child: Text(
+          "Page Number 6",
           style: TextStyle(
             color: Colors.green[900],
             fontSize: 45,
